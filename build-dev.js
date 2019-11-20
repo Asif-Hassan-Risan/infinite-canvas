e4886ec36c110009e8e76868a07775fc4b1cc617
Emile Fokkema <emilefokkema@gmail.com>
2019-11-20 12:24:18 +0100

    add scenario demanding performance

--- build-dev.js
+++ build-dev.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-for(let fileName of ["index.html","index.css", "sandbox.html", "sandbox.css"]){
+for(let fileName of ["index.html","index.css", "sandbox.html", "sandbox.css", "performance.html"]){
 	fs.copyFile("./devtools/"+fileName, "./dist/"+fileName, (err) => {
 		if (err) throw err;
 	});
