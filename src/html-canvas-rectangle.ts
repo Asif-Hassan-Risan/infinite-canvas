5ceb80572e17a7f51687a263cbfcba36e1279d9b
Emile Fokkema <emilefokkema@gmail.com>
2020-09-30 18:22:37 +0200

    begin calculating transformations

--- src/html-canvas-rectangle.ts
+++ src/html-canvas-rectangle.ts
@@ -1,17 +1,36 @@
 import { ConvexPolygon } from "./areas/polygons/convex-polygon";
 import {CanvasRectangle} from "./interfaces/canvas-rectangle";
+import { Transformation } from "./transformation";
 
 export class HTMLCanvasRectangle implements CanvasRectangle{
     public viewboxWidth: number;
     public viewboxHeight: number;
     public polygon: ConvexPolygon;
+    private screenWidth: number;
+    private screenHeight: number;
+    private measuredOnce: boolean = false;
     constructor(private readonly canvasElement: HTMLCanvasElement) {
         this.measure();
     }
     public measure(): void{
-        console.log(`measuring viewbox`);
-        this.viewboxWidth = this.canvasElement.width;
-        this.viewboxHeight = this.canvasElement.height;
+        
+        const viewboxWidth: number = this.canvasElement.width;
+        const viewboxHeight: number = this.canvasElement.height;
+        var rect = this.canvasElement.getBoundingClientRect();
+        const screenWidth: number = rect.width;
+        const screenHeight: number = rect.height;
+        if(viewboxWidth === this.viewboxWidth &&
+            viewboxHeight === this.viewboxHeight &&
+            screenWidth === this.screenWidth &&
+            screenHeight === this.screenHeight){
+                return;
+        }
+        console.log(`viewbox changed`);
+        this.viewboxWidth = viewboxWidth;
+        this.viewboxHeight = viewboxHeight;
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
         this.polygon = ConvexPolygon.createRectangle(0, 0, this.viewboxWidth, this.viewboxHeight);
+        this.measuredOnce = true;
     }
 }
