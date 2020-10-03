9769128d484413c6ea369c6e9589f69013c7b048
Emile Fokkema <emilefokkema@gmail.com>
2020-10-03 17:27:24 +0200

    move absolute and relative transformation to the viewbox transformer

--- src/rectangle/canvas-viewbox-transformer.ts
+++ src/rectangle/canvas-viewbox-transformer.ts
@@ -1,4 +1,5 @@
 import { ConvexPolygon } from "../areas/polygons/convex-polygon";
+import { Instruction } from "../instructions/instruction";
 import { InfiniteCanvasState } from "../state/infinite-canvas-state";
 import { Transformation } from "../transformation";
 import { CanvasRectangle } from "./canvas-rectangle";
@@ -17,5 +18,23 @@ export class CanvasViewboxTransformer implements ViewboxTransformer{
         context.setTransform(1, 0, 0, 1, 0, 0);
         context.rect(-margin, -margin, width, height);
         context.restore();
+    }
+    public transformRelatively(instruction: Instruction): Instruction{
+        return (context: CanvasRenderingContext2D) => {
+            const {a, b, c, d, e, f} = this.rectangle.transformation;
+            context.save();
+            context.transform(a, b, c, d, e, f);
+            instruction(context, this.rectangle.transformation);
+            context.restore();
+        };
+    }
+    public transformAbsolutely(instruction: Instruction): Instruction{
+        return (context: CanvasRenderingContext2D) => {
+            const {a, b, c, d, e, f} = this.rectangle.transformation;
+            context.save();
+            context.setTransform(a, b, c, d, e, f);
+            instruction(context, this.rectangle.transformation);
+            context.restore();
+        };
     }
 }
